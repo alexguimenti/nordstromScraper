c6fb8ad8ed9d12c74e03b4756a92a1f036edda04
alexguimenti <alexguimenti@gmail.com>
2019-04-10 17:43:56 -0300

    getting first results

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,44 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      json: {}
+    };
+  }
+
   componentDidMount() {
     this.fetchProducts();
   }
 
   async fetchProducts() {
     const response = await fetch(
-      "http://localhost:4000/nordstrom?top=3&keyword=red%20dresses"
+      "http://localhost:4000/nordstrom?top=10&keyword=red%20dresses"
     );
     const json = await response.json();
     console.log(json);
+    this.setState({ json });
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    let products = null;
+    if (this.state.json.Products) {
+      products = this.state.json.Products.map(product => {
+        const imgUrl =
+          "https://n.nordstrommedia.com/ImageGallery/store/product/Zoom" +
+          product.Media[0].Path;
+
+        return (
+          <div key={product.SkuId}>
+            <div key={product.SkuId}>{product.Name}</div>
+            <img src={imgUrl} alt="dress" width="300" />
+          </div>
+        );
+      });
+    }
+
+    return <div className="App">{products}</div>;
   }
 }
 
