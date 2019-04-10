eb0efae0739d721393e1646fc6218e084c68ea34
alexguimenti <alexguimenti@gmail.com>
2019-04-10 16:45:46 -0300

    add headers

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const request = require("request-promise").defaults({
+  headers: {
+    Authorization: "apikey 8ea31c48-95c3-4bcf-9db1-d6ada47565f2",
+    NordApiVersion: 1
+  }
+});
 
 const app = express();
 
@@ -6,6 +12,10 @@ app.listen(4000, () => {
   console.log("Server running on port 4000");
 });
 
-app.get("/nordstrom", (req, res, next) => {
-  res.send("Hello World");
+app.get("/nordstrom", async (req, res, next) => {
+  const url =
+    "https://query.ecommerce.api.nordstrom.com/api/queryresults/keywordsearch/?top=1&IncludeFacets=false&Keyword=prom%20dresses";
+  const json = await request.get(url);
+  res.setHeader("Content-Type", "application/json");
+  res.send(json);
 });
